1db7dc9dd7b0ec087c0d57f221b98bfad40c44b2
Andrew Aikman <andrew.aikman@fil.com>
2019-12-24 12:00:15 +0000

    Clean the webpack config of redundant settings

--- site_crawler/visual_diff/webpack.config.js
+++ site_crawler/visual_diff/webpack.config.js
@@ -2,26 +2,6 @@ const path = require('path');
 
 const dev = process.env.NODE_ENV !== 'production'
 
-
-
-
-// module.exports = {
-//   mode: 'development',
-//   entry: './src/index.js',
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: 'main.js'
-//   },
-//   devServer: {
-//     contentBase: path.join(__dirname, 'dist'),
-//     //compress: true,
-//     hot: true,
-//     //index: './index.html',
-//     port: 9000
-//   }
-// };
-
-
 module.exports = {
   mode: dev ? 'development' : 'production',
   entry: {
@@ -49,8 +29,6 @@ module.exports = {
       }
     ]
   },
-  devtool: false,
-  target: "web",
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     //compress: true,
