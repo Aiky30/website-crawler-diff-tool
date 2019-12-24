c5f1730a9a186a0f613d58f73c812741eae0352c
Andrew Aikman <andrew.aikman@fil.com>
2019-12-24 11:50:25 +0000

    Added start of Front end diff visualiser package

---
+++ site_crawler/visual_diff/src/index.js
@@ -0,0 +1,42 @@
+// JQuery (Bootstrap dependency)
+import $ from "jquery";
+
+// Bootstrap
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+console.log("Running JS!");
+
+
+
+/*
+// Get directory for source
+const elementStatsSourcePath = document.querySelector('#diff-stats-source-path');
+
+// Get directory for target
+const elementStatsTargetPath = document.querySelector('#diff-stats-target-path');
+
+// find all matches and misses in the files
+
+// List all of the misses in a table
+const elementTableMisses = document.querySelector('#diff-table-misses');
+
+// List of the matches in a table
+const tableHitsRowTemplate = document.querySelector('#template-diff-table-misses-row');
+const tableHits = document.querySelector('#diff-table-hits');
+const tableHitsTBody = tableHits.querySelector("tbody");
+
+let rowNumber = 1;
+const printTableHitsRow = (rowNumber, source, target) => {
+  console.log("Attempting to add things")
+  tableHitsTBody.appendChild(tableHitsRowTemplate);
+  rowNumber++
+}
+
+printTableHitsRow(rowNumber, "Source text", "Target text")
+
+// Run an initial diff and order a table.
+
+*/
+
