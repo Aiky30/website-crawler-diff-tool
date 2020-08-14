d8058bb3b0b73e60a855f7c31efb81c1bed69a07
Andrew Aikman <andrew.aikman@eliga.services>
2020-08-14 17:48:08 +0100

    Start of a pluggabel site config per crawl, a better file naming structure and folder creation

--- site_crawler/site_crawler/spiders/site_links_spider.py
+++ site_crawler/site_crawler/spiders/site_links_spider.py
@@ -1,24 +1,45 @@
 import scrapy
+
 from datetime import datetime
 from pathlib import Path
+from urllib.parse import urlparse
 
-from scrapy.linkextractors import LinkExtractor
 from slugify import slugify
 
-
-# Generate a directory
-timestamp = datetime.now().timestamp()
-file_root_path = source_dir = Path(Path.cwd(), "crawler_output")
-source_dir = Path(file_root_path, "127.0.0.1-{}".format(timestamp))
-source_dir.mkdir()
+from scrapy.linkextractors import LinkExtractor
 
 
-def write_to_file(filename, contents):
+# TODO: Find a more plugable way to add a site config per site run
+SITE_CONFIG = {
+    'allowed_domains': [
+        '127.0.0.1'
+    ],
+    'start_urls': [
+        'http://127.0.0.1:8000'
+    ],
+    'directory_prefix': '127_0_0_1',
+}
+
+
+def create_result_directory(site):
+    timestamp = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+    file_root_path = Path(Path.cwd(), "crawler_output")
+
+    source_dir = Path(file_root_path, f"{site}-{timestamp}")
+    source_dir.mkdir()
+    return source_dir
+
+
+def generate_filename_from_url(url):
     # Create a filename
-    filename = slugify(filename)
-    filename = "{filename}.html".format(filename=filename)
+    parsed_url = urlparse(url)
+    cleaned_url = slugify(parsed_url.path)
+    return f"{cleaned_url}.html"
 
-    path = Path(source_dir, filename)
+
+def write_to_file(directory, url, contents):
+    filename = generate_filename_from_url(url)
+    path = Path(directory, filename)
 
     # Write to the file
     with open(path, 'wb') as f:
@@ -27,12 +48,12 @@ def write_to_file(filename, contents):
 
 class SiteLinksSpider(scrapy.Spider):
     name = 'site_links'
-    allowed_domains = [
-        '127.0.0.1'
-    ]
-    start_urls = [
-        'http://127.0.0.1:8000/'
-    ]
+    allowed_domains = SITE_CONFIG.get('allowed_domains')
+    start_urls = SITE_CONFIG.get('start_urls')
+
+    def __init__(self, *args, **kwargs):
+        self.result_directory = create_result_directory(SITE_CONFIG.get('directory_prefix'))
+        super().__init__(self, *args, **kwargs)
 
     def parse(self, response):
         page_links = LinkExtractor().extract_links(response)
@@ -42,7 +63,7 @@ class SiteLinksSpider(scrapy.Spider):
         }
 
         # Write the parsed file out
-        write_to_file(response.url, response.body)
+        write_to_file(self.result_directory, response.url, response.body)
 
         for page_link in page_links:
             # CAREFUL: yield scrapy.Request(page_link.url, callback=self.parse, dont_filter=True, headers={('User-Agent', 'Mozilla/5.0')})
