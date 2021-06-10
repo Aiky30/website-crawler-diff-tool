70ca8547c69689d040c55185713885a2d87f45f7
Andrew Aikman <andrew.aikman@eliga.services>
2021-06-10 15:12:48 +0100

    Functioning Sitemap Spider added.

--- site_crawler/site_crawler/spiders/sitemap_spider.py
+++ site_crawler/site_crawler/spiders/sitemap_spider.py
@@ -9,39 +9,17 @@ from site_crawler.utils import create_result_directory, generate_filename_from_u
 class SiteMapSpider(SitemapSpider):
     name = 'sitemap'
     sitemap_urls = SITEMAP_CONFIG['sitemap_urls']
-    login_page = "http://127.0.0.1:8000/en/login/"
 
     def __init__(self, *args, **kwargs):
         self.result_directory = create_result_directory(SITEMAP_CONFIG['directory_prefix'])
         super().__init__(self, *args, **kwargs)
 
-    def start_requests(self):
-        yield scrapy.Request(url=self.login_page, callback=self.login)
-
-        for url in self.sitemap_urls:
-            yield scrapy.Request(url, self._parse_sitemap)
-
-    def login(self, response):
-        """Generate a login request."""
-        return scrapy.FormRequest.from_response(response,
-                    formdata={'name': 'admin', 'password': 'admin'},
-                    callback=self.check_login_response)
-
-    def check_login_response(self, response):
-        """Check the response returned by a login request to see if we are
-        successfully logged in.
-        """
-        if "Hi" in str(response.body):
-            self.log("Successfully logged in. Let's start crawling!")
-            # Now the crawling can begin..
-            return self.initialized()
-        else:
-            # Something went wrong, we couldn't log in, so nothing happens.
-
     def parse(self, response):
 
         print(response.url)
 
-        return
+
+        # Write the parsed file out
+        write_to_file(self.result_directory, response.url, response.body)
 
         # write_to_file(self.result_directory, response.url, response.body)
